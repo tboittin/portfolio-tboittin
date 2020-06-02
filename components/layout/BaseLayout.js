7c6f2cc29717e565c84fb826ab2b0370636c44ff
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-02 19:07:27 +0200

    Proper Layout

--- components/layout/BaseLayout.js
+++ components/layout/BaseLayout.js
@@ -1,11 +1,17 @@
 import Header from "../shared/Header"
 
 const BaseLayout = props => {
+
+    const {className, children } = props;
     return (
-        <>
+        <div className="layout-container">
             <Header />
-            {props.children}
-        </>
+            <main className={`cover ${className}`}>
+                <div className="wrapper">
+                    {children}
+                </div>
+            </main>
+        </div>
     )
 }
 
