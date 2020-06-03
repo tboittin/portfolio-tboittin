60414c74924edbc65ed3a256c077227e5b366733
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-03 07:44:02 +0200

    Header part 1 - rework

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,28 +1,64 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+import Link from 'next/link';
 
-class Header extends React.Component {
-    render () {
-        return(
+
+const BSNavLink = props => {
+    const {title, href} = props
+    return (
+        <Link href={href}>
+            <a className='nav-link'>
+                {title}
+            </a>
+        </Link>
+    )
+}
+
+export default class Header extends React.Component {
+    
+    state = {isOpen : false}
+    toggle = () => this.setState({isOpen: !this.state.isOpen})
+
+    render() {
+        return (
             <>
-                <Link href="/">
-                    <a> Home </a>
-                </Link>
-                <Link href="/about">
-                    <a> About </a>
-                </Link>
-                <Link href="/portfolios">
-                    <a> Portfolios </a>
-                </Link>
-                <Link href="/blogs">
-                    <a> Blogs </a>
-                </Link>
-                <Link href="/cv">
-                    <a> CV </a>
-                </Link>
+              <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.isOpen} navbar>
+                  <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <BSNavLink href="/" title="Home" />
+                    </NavItem>
+                    <NavItem>
+                        <BSNavLink href="/about" title="About" />
+                    </NavItem>
+                    <NavItem>
+                        <BSNavLink href="/portfolios" title="Portfolios" />
+                    </NavItem>
+                    <NavItem>
+                        <BSNavLink href="/blogs" title="Blogs" />
+                    </NavItem>
+                    <NavItem>
+                        <BSNavLink href="/cv" title="CV" />
+                    </NavItem>
+                  </Nav>
+                </Collapse>
+              </Navbar>
             </>
-        )
+          );
     }
 }
-  
-export default Header
