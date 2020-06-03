230625373c0dc9179d46b57bc6c6c024dd667fea
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-03 15:42:36 +0200

    API function - part 2

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -2,18 +2,18 @@
 import BaseLayout from '@/components/layout/BaseLayout'
 import BasePage from '@/components/BasePage'
 
-import axios from 'axios'
-
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-const Portfolios = ({posts}) => {
+const Portfolios = () => {
+
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         async function getPosts() {
             const res = await fetch('/api/v1/posts');
             const data = await res.json();
-            debugger;
+            setPosts(data);
         }
 
         getPosts();
@@ -43,18 +43,6 @@ const Portfolios = ({posts}) => {
             </BasePage>
         </BaseLayout>
     )
-}
-
-Portfolios.getInitialProps = async () => {
-    let posts = []
-    try {
-        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        posts = res.data
-    } catch (e) {
-        console.error(e)
-    }
-
-    return {posts: posts.slice(0,10)}
 }
   
 export default Portfolios
