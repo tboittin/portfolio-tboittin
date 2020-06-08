60597fe6a80805d37c744845530ac84a4a673b01
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-08 11:19:10 +0200

    Env Variables

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import React from 'react'
 import "@/styles/main.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = ({Component, pageProps}) => <Component {...pageProps}/>
+const App = ({Component, pageProps}) => {
+    console.log(process.env.AUTH0_DOMAIN);
+    return <Component {...pageProps}/>
+}
 
 export default App;
