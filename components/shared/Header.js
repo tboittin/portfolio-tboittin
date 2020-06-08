cc9dbd4531993b2ab54c50a778cd2cf70f41d252
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-08 10:34:58 +0200

    Login API Route

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -26,7 +26,8 @@ const BSNavBrand = () =>
     </Link>
 
 
-const LoginLink = () => <span className="nav-link port-navbar-link clickable">Login</span>
+const LoginLink = () => 
+    <BSNavLink href="/api/v1/login" title="Login" />
 
 const LogoutLink = () => <span className="nav-link port-navbar-link clickable">Logout</span>
 
