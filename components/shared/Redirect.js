02fad7daad290d076310f5f12057bcba5824d8f1
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-09 10:10:34 +0200

    Redirect

---
+++ components/shared/Redirect.js
@@ -0,0 +1,17 @@
+
+import {useEffect} from 'react';
+import {useRouter} from 'next/router';
+
+
+
+const Redirect = ({to}) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push(to);
+    }, [])
+    
+    return null;
+}
+
+export default Redirect;
--- pages/secret.js
+++ pages/secret.js
@@ -2,20 +2,17 @@
 import BaseLayout from '@/components/layout/BaseLayout';
 import BasePage from '@/components/BasePage';
 import { useGetUser } from '@/actions/user';
-import { useRouter } from 'next/router';
+import Redirect from '@/components/shared/Redirect';
 
 const Secret = () => {
     const {data, loading } = useGetUser();
-    const router = useRouter();
 
     if (loading) {
         return <p>Loading...</p>
     }
 
     if (!data) {
-        // Todo: improve return
-        router.push('/api/v1/login')
-        return null;
+        return <Redirect to="/api/v1/login" />
     } else {
         return (
             <BaseLayout user={data} loading={loading}>
