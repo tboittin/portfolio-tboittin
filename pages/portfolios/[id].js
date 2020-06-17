f7a543da89097acb3a039d1f2fad9747b58fcb44
Thomas Boittin <tboittin.pro@gmail.com>
2020-06-17 21:47:43 +0200

    Get static paths

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -19,11 +19,32 @@ const Portfolio = ({portfolio}) => {
     )
 }
 
-export async function getServerSideProps({query}) {
-    const json = await new PortfolioApi().getById(query.id);
-    const portfolio = json.data;
+// export async function getServerSideProps({query}) {
+//     const json = await new PortfolioApi().getById(query.id);
+//     const portfolio = json.data;
 
-    return {props: {portfolio}}
+//     return {props: {portfolio}}
+// }
+
+export async function getStaticPaths() {
+    const json = await new PortfolioApi().getAll();
+    const portfolios = json.data;
+
+    // Get the paths we want to pre-render based on portfolio ID
+    const paths = portfolios.map(portfolio =>{
+        return {
+            params: {id: portfolio._id}
+        }
+    })
+
+    // fallback: false means that "not found pages" will be resolved into 404 page
+    return {paths, fallback: false};
+}
+
+export async function getStaticProps({params}) {
+    const json = await new PortfolioApi().getById(params.id);
+    const portfolio = json.data;
+    return { props: {portfolio}}
 }
 
 export default Portfolio
