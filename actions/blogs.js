7df051853260998d6bb4a3d5d36063f69381e2cb
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-05 18:15:44 +0200

    153. Create blog from client

---
+++ actions/blogs.js
@@ -0,0 +1,9 @@
+
+import axios from 'axios';
+import {useApiHandler} from '@/actions'
+
+const createBlog = (data) => axios.post('/api/v1/blogs', data);
+
+
+export const useCreateBlog = () => useApiHandler(createBlog);
+
