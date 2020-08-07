604f663b41b7f34504a2edf158d3508580410fb5
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-07 19:59:06 +0200

    155. Get initial content to blog update

--- actions/blogs.js
+++ actions/blogs.js
@@ -1,9 +1,15 @@
 
 import axios from 'axios';
 import {useApiHandler} from '@/actions'
+import useSWR from "swr"
+import {fetcher} from '@/actions'
 
 const createBlog = (data) => axios.post('/api/v1/blogs', data);
 
 
 export const useCreateBlog = () => useApiHandler(createBlog);
 
+export const useGetBlog = (id) => {
+    const { data, error, ...rest} = useSWR(id ? '/api/v1/blogs/'+id : null, fetcher);
+    return { data, error, loading: !data && !error, ...rest}
+}
--- pages/blogs/editor/[id].js
+++ pages/blogs/editor/[id].js
@@ -3,12 +3,23 @@ import BasePage from "@/components/BasePage";
 import withAuth from "@/hoc/withAuth";
 import { Editor } from "slate-simple-editor";
 import { toast } from "react-toastify";
+import { useGetBlog } from "actions/blogs";
+import { useRouter } from "next/router";
 
 const BlogUpdateEditor = ({ user, loading }) => {
+  const router = useRouter()
+  const {data} = useGetBlog(router.query.id);
+
   return (
     <BaseLayout user={user} loading={loading}>
       <BasePage>
-        <Editor header="Update your Blog"/>
+        {data && data.content &&
+          <Editor 
+            header="Update your Blog"
+            initialContent={data.content}
+            onSave={()=>{}}
+          />
+        }
       </BasePage>
     </BaseLayout>
   );
