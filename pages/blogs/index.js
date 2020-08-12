fab5a3772ac25483c2736ed42cdb9a1643cfd4b9
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-12 18:56:00 +0200

    170. Display blogs

---
+++ pages/blogs/index.js
@@ -0,0 +1,44 @@
+import BaseLayout from "@/components/layout/BaseLayout";
+import BasePage from "@/components/BasePage";
+import { useGetUser } from "@/actions/user";
+import { Row, Col} from "reactstrap";
+import Masthead from "components/shared/Masthead";
+import BlogApi from 'lib/api/blogs';
+import BlogItem from "components/BlogItem";
+
+const Blogs = ({blogs}) => {
+  const { data, loading } = useGetUser();
+  debugger
+  return (
+    <BaseLayout
+      navClass="transparent"
+      className="blog-listing-page"
+      user={null}
+      loading={false}
+    >
+      <Masthead imagePath="/images/home-bg.jpg">
+        <h1>Fresh Blogs</h1>
+        <span className="subheading">Programming, travelling...</span>
+      </Masthead>
+      <BasePage className="blog-body">
+        <Row>
+          {blogs.map(blog => 
+            <Col key={blog._id} md="10" lg="8" className="mx-auto">
+              <BlogItem blog={blog}/>
+            </Col>
+          )}
+        </Row>
+      </BasePage>
+    </BaseLayout>
+  );
+};
+
+export async function getStaticProps() {
+    const json = await new BlogApi().getAll()
+    return {
+        props: {blogs: json.data},
+        unstable_revalidate: 60
+    }
+}
+
+export default Blogs;
