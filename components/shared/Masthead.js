d9cc3e7eb6850bae00e2db8e7b7742d1e3eae071
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-12 18:34:02 +0200

    169. Blog index page

--- components/shared/Masthead.js
+++ components/shared/Masthead.js
@@ -1,7 +1,7 @@
 import { Container, Row, Button } from "reactstrap";
 import Link from "next/link";
 
-const Masthead = ({imagePath}) => (
+const Masthead = ({children, imagePath}) => (
   <div
     className="masthead"
     style={{ backgroundImage: `url(${imagePath})` }}
@@ -11,13 +11,7 @@ const Masthead = ({imagePath}) => (
       <Row>
         <div className="col-lg-8 col-md-10 mx-auto">
           <div className="site-heading">
-            <h1>Blogs Dashboard</h1>
-            <span className="subheading">
-              Let's write some nice blog today{" "}
-              <Link href="/blogs/new">
-                <Button color="primary">Create a new Blog</Button>
-              </Link>
-            </span>
+            {children}
           </div>
         </div>
       </Row>
