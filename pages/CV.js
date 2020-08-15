ceba80e1c1ab91685bb0274714fffe8122887449
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-15 19:21:28 +0200

    179. CV Page

--- pages/CV.js
+++ pages/CV.js
@@ -2,13 +2,18 @@
 import BaseLayout from '@/components/layout/BaseLayout'
 import BasePage from '@/components/BasePage'
 import { useGetUser } from '@/actions/user'
+import { Row, Col } from 'reactstrap';
 
 const CV = () => {
     const { data, loading } = useGetUser();
     return(
         <BaseLayout user={data} loading={loading}>
             <BasePage>
-                <h1>Hi I'm the CV page!</h1>
+                <Row>
+                    <Col md={{size: 8, offset:2}}>
+                        <iframe style={{width:'100%', height:'800px'}} src="CV - Thomas Boittin - 25-05-20.pdf"/>
+                    </Col>
+                </Row>
             </BasePage>
         </BaseLayout>
     )
