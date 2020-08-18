7f53a1a1639c8cf1da5cfda128de41f1eb4b5c39
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-18 12:14:52 +0200

    190. Open graph

--- components/BasePage.js
+++ components/BasePage.js
@@ -1,12 +1,14 @@
 import { Container } from "reactstrap";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const BasePage = (props) => {
+  const router = useRouter();
   const {
     indexPage,
     className = "",
     header,
-    title = 'Website',
+    title = "Website",
     children,
     metaDescription = "My name is Thomas Boittin and I am a freelance web-developer. Have a look at my work !",
   } = props;
@@ -16,15 +18,25 @@ const BasePage = (props) => {
       <Head>
         <title>{title + " - Thomas Boittin"}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={metaDescription} key="description" />
+        <meta name="title" content={title} key="title" />
+        <meta property="og:title" content={title} key="og:title" />
+        <meta property="og:locale" content="en_EU" key="og:locale" />
         <meta
-          name="description"
+          property="og:url"
+          content={process.env.BASE_URL + router.asPath}
+          key="og:url"
+        />
+        <meta property="og:type" content="website" key="og:type" />
+        <meta
+          property="og:description"
           content={metaDescription}
-          key="description"
+          key="og:description"
         />
         <meta
-          name="title"
-          content={title}
-          key="title"
+          property="og:image"
+          content={process.env.BASE_URL + "/images/section-1.png"}
+          key="og:image"
         />
       </Head>
       <div className={pageType + " " + className}>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 module.exports = {
     env: {
-        AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
+        AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
+        BASE_URL: process.env.BASE_URL
     }
 }
