c2f5e1c4dd95af8e1d589ecf37bdaa6c9deb6409
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-18 09:25:37 +0200

    188. Index page type

--- components/BasePage.js
+++ components/BasePage.js
@@ -1,30 +1,27 @@
 import { Container } from "reactstrap";
-import Head from 'next/head';
+import Head from "next/head";
 
+const BasePage = (props) => {
+  const { indexPage, className = "", header, title, children } = props;
+  const pageType = indexPage ? 'index-page' : 'base-page'
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>{title + " - Thomas Boittin"}</title>
+      </Head>
+      <div className={pageType + ' ' + className}>
+        <Container>
+          {header && (
+            <div className="page-header">
+              <h1 className="page-header-title">{header}</h1>
+            </div>
+          )}
+          {children}
+        </Container>
+      </div>
+    </>
+  );
+};
 
-const BasePage = props => {
-    const {className = '', header, title, children} = props;
-    return (
-      <>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <title>{title+' - Thomas Boittin'}</title>
-        </Head>
-        <div className={"base-page " + className}>
-          <Container>
-            {header && (
-              <div className="page-header">
-                <h1 className="page-header-title">{header}</h1>
-              </div>
-            )}
-            {children}
-          </Container>
-        </div>
-      </>
-    );
-}
-
-export default BasePage
+export default BasePage;
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Typed from "react-typed";
 
 import { useGetUser } from "@/actions/user";
 import { useState, useRef, useEffect } from "react";
+import BasePage from "components/BasePage";
 
 const ROLES = ["Developer", "Tech Lover", "Team Player", "React.js", "Next.js"];
 
@@ -16,85 +17,89 @@ const Index = () => {
 
   useEffect(() => {
     startAnimation();
-    return () => flipInterval.current && clearInterval(flipInterval.current)
-  }, [])
+    return () => flipInterval.current && clearInterval(flipInterval.current);
+  }, []);
 
   const startAnimation = () => {
     flipInterval.current = setInterval(() => {
-      setisFlipping((prevFlipping) => !prevFlipping)
+      setisFlipping((prevFlipping) => !prevFlipping);
     }, 30000);
-  }
+  };
 
   return (
     <BaseLayout
       user={data}
       loading={loading}
       navClass="transparent"
-      className={"cover"+ (isFlipping ? ' cover-orange' : ' cover-blue')}
+      className={"cover" + (isFlipping ? " cover-orange" : " cover-blue")}
     >
-      <div className="main-section">
-        <div className="background-image">
-          <img src="/images/background-index.png" />
-        </div>
+      <BasePage indexPage title="Home">
+        <div className="main-section">
+          <div className="background-image">
+            <img src="/images/background-index.png" />
+          </div>
 
-        <Container>
-          <Row>
-            <Col md="6">
-              <div className="hero-section">
-                <div className={`flipper`+ (isFlipping ? ' isFlipping' : '') }>
-                  <div className="front">
-                    <div className="hero-section-content">
-                      <h2> Full Stack Web Developer </h2>
-                      <div className="hero-section-content-intro">
-                        Have a look at my portfolio and job history.
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div
+                    className={`flipper` + (isFlipping ? " isFlipping" : "")}
+                  >
+                    <div className="front">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
+                      </div>
+                      <img className="image" src="/images/section-1.png" />
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
                       </div>
                     </div>
-                    <img className="image" src="/images/section-1.png" />
-                    <div className="shadow-custom">
-                      <div className="shadow-inner"> </div>
-                    </div>
-                  </div>
-                  <div className="back">
-                    <div className="hero-section-content">
-                      <h2> Full Stack Web Developer </h2>
-                      <div className="hero-section-content-intro">
-                        Have a look at my portfolio and job history.
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
                       </div>
-                    </div>
-                    <img className="image" src="/images/section-2.png" />
-                    <div className="shadow-custom-orange">
-                      <div className="shadow-inner"> </div>
+                      <img className="image" src="/images/section-2.png" />
+                      <div className="shadow-custom-orange">
+                        <div className="shadow-inner"> </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Col>
-            <Col md="6" className="hero-welcome-wrapper">
-              <div className="hero-welcome-text">
-                <h1>
-                  Welcome to the portfolio website of Filip Jerga. Get informed,
-                  collaborate and discover projects I was working on through the
-                  years!
-                </h1>
-              </div>
-              <Typed
-                loop
-                strings={ROLES}
-                typeSpeed={70}
-                backSpeed={70}
-                backDelay={1000}
-                loopCount={0}
-                showCursor
-                className="self-typed"
-                cursorChar="|"
-              ></Typed>
-              <div className="hero-welcome-bio">
-                <h1>Let's take a look on my work.</h1>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>
+                    Welcome to the portfolio website of Filip Jerga. Get
+                    informed, collaborate and discover projects I was working on
+                    through the years!
+                  </h1>
+                </div>
+                <Typed
+                  loop
+                  strings={ROLES}
+                  typeSpeed={70}
+                  backSpeed={70}
+                  backDelay={1000}
+                  loopCount={0}
+                  showCursor
+                  className="self-typed"
+                  cursorChar="|"
+                ></Typed>
+                <div className="hero-welcome-bio">
+                  <h1>Let's take a look on my work.</h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </BasePage>
     </BaseLayout>
   );
 };
