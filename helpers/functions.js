3b631eb1b1c47ea0a69922116531d73c41c0c0a6
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-26 12:26:33 +0200

    add limitTextLength function to helpers

--- helpers/functions.js
+++ helpers/functions.js
@@ -5,3 +5,11 @@ import localization from "moment/locale/fr";
 moment.updateLocale("en", localization);
 
 export const formatDate = (date, dateFormat = 'FF') => moment(date).format(dateFormat)
+
+export const limitTextLength = (text, length) => {
+    if (text.length > length) {
+        return text.substr(0, length-3)+"...";
+    } else {
+        return text;
+    }
+}
