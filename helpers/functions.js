cfff7c0cd9c580197f42485b07a6b0c62ca003bf
Thomas Boittin <tboittin.pro@gmail.com>
2020-08-26 08:57:34 +0200

    Fix date format

--- helpers/functions.js
+++ helpers/functions.js
@@ -2,6 +2,6 @@ import moment from "moment";
 
 // setup the fr parameter in moment.js to get the date format right
 import localization from "moment/locale/fr";
-moment.updateLocale("fr", localization);
+moment.updateLocale("en", localization);
 
 export const formatDate = (date, dateFormat = 'FF') => moment(date).format(dateFormat)
