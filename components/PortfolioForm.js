c5b69d1f158c9fd2949a9545bdb159ad517054e4
Thomas Boittin <tboittin.pro@gmail.com>
2020-09-02 11:57:59 +0200

    update of portfolio form

--- components/PortfolioForm.js
+++ components/PortfolioForm.js
@@ -1,13 +1,10 @@
 
 
-import DatePicker from "react-datepicker";
 import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
 
 
 const Portfolioform = ({onSubmit, initialData = {}}) => {
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
     const {register, handleSubmit, setValue } = useForm({defaultValues: initialData});
 
     useEffect(() => {
@@ -20,22 +17,6 @@ const Portfolioform = ({onSubmit, initialData = {}}) => {
         if (startDate) { setStartDate(new Date(startDate))}
         if (endDate) { setEndDate(new Date(endDate))}
     }, [initialData])
-
-    const handleStartDate = (date) => {
-        setStartDate(date);
-        setValue('startDate', date);
-    }
-
-    const handleEndDate = (date) => {
-        setEndDate(date);
-        setValue('endDate', date);
-    }
-
-    const handleDateChange = (dateType, setDate) => date => {
-        
-        setValue(dateType, date);
-        setDate(date);
-    }
     
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -51,48 +32,17 @@ const Portfolioform = ({onSubmit, initialData = {}}) => {
             </div>
 
             <div className="form-group">
-                <label htmlFor="city">Company</label>
+                <label htmlFor="city">Project Link</label>
                 <input
                     ref={register}
-                    name="company"
+                    name="projectLink"
                     type="text"
                     className="form-control"
-                    id="company"
+                    id="projectLink"
                 />
             </div>
-
-            <div className="form-group">
-                <label htmlFor="city">Company Website</label>
-                <input
-                    ref={register}
-                    name="companyWebsite"
-                    type="text"
-                    className="form-control"
-                    id="companyWebsite"
-                />
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="street">Location</label>
-                <input
-                    ref={register}
-                    name="location"
-                    type="text"
-                    className="form-control"
-                    id="location"
-                />
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="street">Job Title</label>
-                <input
-                    ref={register}
-                    name="jobTitle"
-                    type="text"
-                    className="form-control"
-                    id="jobTitle"
-                />
-            </div>
+            
+            {/* TODO insert Image */}
 
             <div className="form-group">
                 <label htmlFor="description">Description</label>
@@ -105,52 +55,7 @@ const Portfolioform = ({onSubmit, initialData = {}}) => {
                     id="description">
                 </textarea>
             </div>
-
-            <div className="form-group">
-                <label htmlFor="startDate">Start Date</label>
-                <div>
-                    <DatePicker
-                        showYearDropdown
-                        dateFormat="dd/MM/yyyy"
-                        selected={startDate} 
-                        onChange={handleDateChange('startDate', setStartDate)} 
-                    />
-                </div>
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="endDate">End Date</label>
-                <div>
-                    <DatePicker
-                        disabled={!endDate}
-                        showYearDropdown
-                        dateFormat="dd/MM/yyyy"
-                        selected={endDate} 
-                        onChange={handleDateChange('endDate', setEndDate)} 
-                    />
-                </div>
-            </div>
-            <div className="form-group">
-                { endDate &&
-                    <button
-                        type="button"
-                        className="btn btn-danger"
-                        onClick={()=> handleDateChange('endDate', setEndDate)(null)}
-                    >
-                        No End Date
-                    </button>
-                }
-                { !endDate &&
-                    <button
-                        type="button"
-                        className="btn btn-success"
-                        onClick={() => handleDateChange('endDate', setEndDate)(new Date(new Date().setHours(0,0,0,0)))}
-                    >
-                        Set End Date
-                    </button>
-                }
-                
-            </div>
+            
             <button
                 type="submit"
                 className="btn btn-primary"
