49e7b309f00acd9295c51b2ab359b35c546a1073
Thomas Boittin <tboittinpro@gmail.com>
2020-11-17 17:36:36 +0400

    adding QSIE image

--- components/PortfolioCards.js
+++ components/PortfolioCards.js
@@ -1,22 +1,28 @@
-import { Card, CardBody, CardText, CardTitle, CardImg, CardImgOverlay, Container } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardText,
+  CardTitle,
+  CardImg,
+  CardImgOverlay,
+  Container,
+} from "reactstrap";
 import { limitTextLength } from "helpers/functions";
 
 export const PortfolioCard = ({ portfolio, children }) => (
   <Card className="portfolio-card">
     {!portfolio.image && (
       <>
-        {/* <CardHeader className="portfolio-card-header">
-          {portfolio.jobTitle}
-        </CardHeader> */}
         <CardBody>
-          <p className="portfolio-card-city">{portfolio.location}</p>
-          <CardTitle className="portfolio-card-title">
+          <Container className="px-0 h-50">
+            <CardTitle className="portfolio-card-title">
               {limitTextLength(portfolio.title, 30)}
-          </CardTitle>
-          <CardText className="portfolio-card-text">
+            </CardTitle>
+            <CardText className="portfolio-card-text">
               {limitTextLength(portfolio.description, 100)}
-          </CardText>
-          {children}
+            </CardText>
+            {children}
+          </Container>
         </CardBody>
       </>
     )}
