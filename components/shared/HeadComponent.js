dd1ca7adadd8a29aa790ffaf1f0102d12917cd9f
Thomas Boittin <tboittin@gmail.com>
2021-04-20 21:05:44 +0200

    index SEO

---
+++ components/shared/HeadComponent.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+
+export const HeadComponent = ({ title, description, url, image, imageURL,imageWidth, imageHeight }) => {
+  return (
+    <>
+      <title>{title ? title : 'Thomas Boittin Website'}</title>
+      <meta property="og:locale" content="en_US" key="locale" />
+      <meta
+        property="og:locale:alternate"
+        content="fr_FR"
+        key="locale:alternate"
+      />
+      <meta property="og:type" content="website" key="type" />
+      <meta property="og:title" content={title ? title : 'Thomas Boittin Website'} key="title" />
+      <meta property="og:description" content={description} key="description" />
+      <meta property="og:url" content={url} key="url" />
+      <meta property="og:site_name" content="Thomas Boittin" key="site_name" />
+      <meta property="og:image" content={image ? image : 'https://tboittin.vercel.app/images/section-1.png'} key="image" />
+      <meta property="og:image:secure_url" content={imageURL ? imageURL : 'https://tboittin.vercel.app/images/section-1.png'} key="imageURL" />
+      <meta property="og:image:width" content={imageWidth ? imageWidth : 838} key="imageWidth" />
+      <meta property="og:image:height" content={imageHeight ? imageHeight : 1364} key="imageHeight" />
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        key="viewport"
+      />
+    </>
+  )
+}
--- data.js
+++ data.js
@@ -1,13 +1,15 @@
 
-
-
-export const data = [
-    {
-        title: 'Hello World',
-        subtitle: 'Hello World subtitle'
+export const indexData = {
+    head: {
+        title: 'Thomas Boittin Website',
+        description: 'Welcome to Thomas Boittin website - ReactJS and NextJS developer',
+        url: 'https://tboittin.vercel.app/',
     },
-    {
-        title: 'Hello World 2',
-        subtitle: 'Hello World subtitle 2'
-    },
-]
+    intro: ["Welcome to Thomas Boittin's website ! I put here my projects and some cool stuff !"],
+    roles: ['React.js dev',
+    'Next.js dev',
+    'Fast Learner',
+    'Web enthousiast',
+    'Casual Gamer',
+    'Nerd',],
+}
