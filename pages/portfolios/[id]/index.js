8d2aae093284f25752e1b893fe56cf9009ff73b6
Thomas Boittin <tboittin@gmail.com>
2021-04-21 17:35:34 +0200

    Portfolio Item SEO

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -1,80 +1,91 @@
-import BaseLayout from "@/components/layout/BaseLayout";
-import BasePage from "@/components/BasePage";
+import BaseLayout from '@/components/layout/BaseLayout'
+import BasePage from '@/components/BasePage'
+import { HeadComponent } from 'components/shared/HeadComponent'
 
-import { useGetUser } from "@/actions/user";
-import PortfolioApi from "@/lib/api/portfolios";
+import { useGetUser } from '@/actions/user'
+import PortfolioApi from '@/lib/api/portfolios'
 
-import { formatDate } from "helpers/functions";
-import { useRouter } from "next/router";
-import { Container } from "reactstrap";
+import { useRouter } from 'next/router'
+import { Container } from 'reactstrap'
 
 const Portfolio = ({ portfolio }) => {
-  const { data: dataU, loading: loadingU } = useGetUser();
-  const router = useRouter();
+  const { data: dataU, loading: loadingU } = useGetUser()
+  const router = useRouter()
 
   if (router.isFallback) {
-    return <h1>Your page is getting served</h1>;
+    return <h1>Your page is getting served</h1>
   }
 
   return (
-    <BaseLayout user={dataU} loading={loadingU} navClass="transparent">
-      <BasePage
-        indexPage
-        noWrapper
-        title={portfolio.title}
-        metaDescription={portfolio.description}
-      >
-        <div className="portfolio-detail">
-          <div className="cover-container d-flex h-100 w-100 p-3 mx-auto flex-column">
-            <main role="main" className="inner page-cover">
-              {!router.isFallback && (
-                <>
-                  <h1 className="cover-heading">{portfolio.title}</h1>
-                  <Container>
-                    {portfolio.image &&
-                      <img src={'/upload/mockup/'+portfolio.image} className="w-50 text-center mb-2"/>
-                    }
-                  </Container>
-                  
-                  <p className="lead mx-5">{portfolio.description}</p>
-                  <p className="lead">
-                    <a
-                      href={portfolio.projectLink}
-                      target="_"
-                      className="btn btn-lg btn-secondary"
-                    >
-                      Have a look !
-                    </a>
-                  </p>
-                </>
-              )}
-            </main>
+    <>
+      <HeadComponent
+        title={'Thomas Boittin - '+portfolio.title}
+        description={portfolio.description}
+        url={`https://tboittin.vercel.app/portfolios/${portfolio.id}`}
+        image={portfolio.image}
+      />
+      <BaseLayout user={dataU} loading={loadingU} navClass="transparent">
+        <BasePage
+          indexPage
+          noWrapper
+          title={portfolio.title}
+          metaDescription={portfolio.description}
+        >
+          <div className="portfolio-detail">
+            <div className="cover-container d-flex h-100 w-100 p-3 mx-auto flex-column">
+              <main role="main" className="inner page-cover">
+                {!router.isFallback && (
+                  <>
+                    <h1 className="cover-heading">{portfolio.title}</h1>
+                    <Container>
+                      {portfolio.image && (
+                        <img
+                          src={'/upload/mockup/' + portfolio.image}
+                          className="w-50 text-center mb-2"
+                        />
+                      )}
+                    </Container>
+
+                    <p className="lead mx-5">{portfolio.description}</p>
+                    <p className="lead">
+                      <a
+                        href={portfolio.projectLink}
+                        target="_"
+                        className="btn btn-lg btn-secondary"
+                      >
+                        Have a look !
+                      </a>
+                    </p>
+                  </>
+                )}
+              </main>
+            </div>
           </div>
-        </div>
-      </BasePage>
-    </BaseLayout>
-  );
-};
+        </BasePage>
+      </BaseLayout>
+    </>
+  )
+}
 
 export async function getStaticPaths() {
-  const json = await new PortfolioApi().getAll();
-  const portfolios = json.data;
+  const json = await new PortfolioApi().getAll()
+  const portfolios = json.data
 
   // Get the paths we want to pre-render based on portfolio ID
   const paths = portfolios.map((portfolio) => {
     return {
       params: { id: portfolio._id },
-    };
-  });
+    }
+  })
 
   // fallback: false means that "not found pages" will be resolved into 404 page
-  return { paths, fallback: true };
+  return { paths, fallback: true }
 }
 
 export async function getStaticProps({ params }) {
-  const json = await new PortfolioApi().getById(params.id);
-  const portfolio = json.data;
-  return { props: { portfolio }, unstable_revalidate: 60 };
+  const json = await new PortfolioApi().getById(params.id)
+  const portfolio = json.data
+  return { props: { portfolio }, unstable_revalidate: 60 }
 }
 
-export default Portfolio;
+export default Portfolio
